99cce5ca2f7e15e90e6446db09f943108c4d7dd4
alina.vagapova <aivagapova96@gmail.com>
2022-07-16 11:58:26 +0300

    Ввели изменения путем формата строки, добавили преобразование

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 name = input('Enter your name:')
 surname = input('Enter your surname:')
-print('Hello, your name is {}, your surname is {}'.format(name, surname))
+age = int(input('Enter your age:'))
+print(age, type(age))
+print('Hello, your name is {user_name}, your surname is {surname}, your age is {user_age}'.format(user_name=name, surname=surname, user_age=age))
 
