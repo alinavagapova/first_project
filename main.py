dd0bd64156c4d53d7eb5c1b91ad22dbb204f4ef2
alina.vagapova <aivagapova96@gmail.com>
2022-07-16 12:04:23 +0300

    f-строки

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 name = input('Enter your name:')
 surname = input('Enter your surname:')
 age = int(input('Enter your age:'))
-print(age, type(age))
-print('Hello, your name is {user_name}, your surname is {surname}, your age is {user_age}'.format(user_name=name, surname=surname, user_age=age))
-
+# print(age, type(age))
+print(f'Hello, your name is {name}, your surname is {surname}, your age is {age}')
