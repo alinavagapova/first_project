5a7c8bde80093d18bffa52d04bd4948f1e3d91ff
alina.vagapova <aivagapova96@gmail.com>
2022-07-17 17:33:29 +0300

    Создать произвольный список из целочисленных значений от 0 до 20(вкл.)     Пройтись по этому списку в цикле     и принтовать значние если оно нечетное ( i is odd)     и принтовать значние если оно четное ( i is even)     используя f-строки

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-name = input('Enter your name:')
-surname = input('Enter your surname:')
-age = int(input('Enter your age:'))
-# print(age, type(age))
-print(f'Hello, your name is {name}, your surname is {surname}, your age is {age}')
-print('I am testing a branche')
+import numpy as np
+# test_list = np.random.randint(0, 20, 10)
+# print(test_list)
+# вопрос - как создать произвольного размера список?
+test_list = np.random.choice(20, 10)
+print(test_list)
+for i in range(len(test_list)):
+    if (test_list[i]) % 2 == 0:
+        print(i, f'{test_list[i]} is even')
+    else:
+        print(i, f'{test_list[i]} is odd')
