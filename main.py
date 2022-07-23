3d0fe76ff08ff0542dc43916e340ee322b43306a
alina.vagapova <aivagapova96@gmail.com>
2022-07-23 12:38:09 +0300

    разбор дз

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
 import matplotlib.pyplot as plt
 import numpy as np
-
-y = lambda x: x
+y = lambda x: x**2
 fig = plt.subplots()
-x = np.linspace(0, 5, 100)
-plt.plot(x, y(x))
+x = [0, 2, 4, 6]
+z = [1, 3, 5, 7]
+print(x, z)
+plt.plot(x, z, color='r', linestyle=':')
+plt.plot(x, list(map(y, x)))
+plt.title('График')
 ax = plt.gca()
 ax.set_xlabel("Ось X", fontsize=15, color='red')
 ax.set_ylabel("Ось Y", fontsize=15, color='green')
+plt.savefig('graph.png')
 plt.show()
